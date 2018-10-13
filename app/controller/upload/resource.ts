435964ee2485eb3b5abd18e0aea50f99b91ce287
qingfengwuyou <h3658686@126.com>
2018-10-13 18:30:01 +0800

    no message

--- app/controller/upload/resource.ts
+++ app/controller/upload/resource.ts
@@ -3,6 +3,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as uuid from 'uuid';
 
+const fsPromise = fs.promises;
+
 import awaitStream = require('await-stream-ready');
 import sendToWormhole = require('stream-wormhole');
 export default class ResourceController extends Controller {
@@ -20,9 +22,14 @@ export default class ResourceController extends Controller {
     ctx.logger.info('upload:', '\n源:', stream.filename, '\n目标:', target);
     const writeStream = fs.createWriteStream(target);
     const awaitWriteStream = awaitStream.write;
+
     try {
       await awaitWriteStream(stream.pipe(writeStream));
       ctx.logger.info(target, ' 保存成功.');
+
+      const stat = await fsPromise.stat(target);
+      ctx.logger.info(stat);
+
       ctx.body = { ok: 1 };
     } catch (err) {
       await sendToWormhole(stream);
